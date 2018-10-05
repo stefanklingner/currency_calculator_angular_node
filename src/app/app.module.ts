85d531c93920ce609765e58b04bbc1b35a5beaf9
stefan klingner <stefan_klingner@outlook.com>
2018-10-05 20:42:57 +0200

    erste komponente. ng-for fehlt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CurrenciesComponent } from './currencies/currencies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrenciesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/currencies/currencies.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-currencies',
+  templateUrl: './currencies.component.html',
+  styleUrls: ['./currencies.component.css']
+})
+export class CurrenciesComponent implements OnInit {
+
+  currencies: [
+    { name: 'eur', sign: '€', value: '' },
+    { name: 'usd', sign: '$', value: '' },
+    { name: 'btc', sign: 'Btc', value: '' },
+    { name: 'eth', sign: 'Eth', value: '' },
+    { name: 'ltc', sign: 'Ltc', value: '' },
+    { name: 'iota', sign: 'Iota', value: '' }]
+
+  constructor() { }
+
+  ngOnInit() {
+
+    console.log(" initializing currencies component");
+
+  }
+
+}
