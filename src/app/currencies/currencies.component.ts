87cfadcd34da4b0da8e308111164b499345d5065
stefan klingner <stefan_klingner@outlook.com>
2018-10-07 20:25:48 +0200

    added currency list

--- src/app/currencies/currencies.component.ts
+++ src/app/currencies/currencies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-currencies',
   templateUrl: './currencies.component.html',
@@ -7,13 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrenciesComponent implements OnInit {
 
-  currencies: [
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+
+  currencies = [
     { name: 'eur', sign: '€', value: '' },
     { name: 'usd', sign: '$', value: '' },
     { name: 'btc', sign: 'Btc', value: '' },
     { name: 'eth', sign: 'Eth', value: '' },
     { name: 'ltc', sign: 'Ltc', value: '' },
-    { name: 'iota', sign: 'Iota', value: '' }]
+    { name: 'iota', sign: 'Iota', value: '' }
+  ];
+
+    name : string = "stefan";
 
   constructor() { }
 
